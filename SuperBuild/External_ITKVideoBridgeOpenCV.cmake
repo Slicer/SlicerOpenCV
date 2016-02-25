ba6580f220e810e1523bc638ac61b755c0e6fa4c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-02-24 23:49:13 -0500

    COMP: Reorganize build system to follow SuperBuild extension templates. Fixes #3
    See <Slicer_SOURCE_DIR>/Extensions/Testing/SuperBuildExtensionTemplate
    
    Also applies the following changes:
    
    1) OpenCV external project:
    
    * As reported in the OpenCV sample project, change variable from
    opencv_* to OpenCV_*. See https://github.com/Itseez/opencv/blob/ddf82d0b154873510802ef75c53e628cd7b2cb13/samples/cpp/example_cmake/CMakeLists.txt
    
    * Speed up source checkout by downloading source archive instead of cloning
    the full project.
    
    * Add support for build option -DOpenCV_SOURCE_DIR=/path/to/source/opencv.
    This will make the extension build system development and testing faster
    by avoiding checking the OpenCV sources for each clean build.
    
    2) ITKVideoBridgeOpenCV external project:
    
    * Change "bridgeopencv" to "ITKVideoBridgeOpenCV". This matches the name of
    the ITK module.
    
    * Do not force build of ITKVideoBridgeOpenCV to Debug
    
    * Do not build ITKVideoBridgeOpenCV tests
    
    * Do not associate ITKVideoBridgeOpenCV source directory with FIND_PACKAGE
    label. This is reserved for build directory that should be packaged.
    
    * Do not install ITKVideoBridgeOpenCV at build time. Using find_package
    with ITK by specifying the COMPONENTS is sufficient.
    
    3) SlicerOpenCV Superbuild:
    
    * Required CMake >= 3.1 and specify "BUILD_ALWAYS 1" to ensure incremental
    build will reconfigure and build the inner project. Slicer extension
    templates have also been fixed to specify "BUILD_ALWAYS 1"
    
    4) SlicerOpenCV inner build:
    
    * Update logic to use:
    
       find_package(ITK COMPONENTS ITKVideoBridgeOpenCV REQUIRED)
       include(${ITK_USE_FILE})
    
      Doing so allow to use ${ITK_LIBRARIES} and ${ITK_INCLUDE_DIRS}
    
    Fixes #3
    Fixes #5

---
+++ SuperBuild/External_ITKVideoBridgeOpenCV.cmake
@@ -0,0 +1,48 @@
+#-----------------------------------------------------------------------------
+# Build the ITK OpenCV bridge, pointing it to Slicer's ITK and this build
+# of OpenCV
+
+set(proj ITKVideoBridgeOpenCV)
+
+# Dependencies
+set(${proj}_DEPENDENCIES OpenCV)
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+set(ITK_SOURCE_DIR ${ITK_DIR}/../ITKv4)
+set(${proj}_SOURCE_DIR ${ITK_SOURCE_DIR}/Modules/Video/BridgeOpenCV)
+ExternalProject_Message(${proj} "ITK_SOURCE_DIR:${ITK_SOURCE_DIR}")
+ExternalProject_Message(${proj} "${proj}_SOURCE_DIR:${${proj}_SOURCE_DIR}")
+
+# don't allow using the system ITK, use the Slicer one
+set(${proj}_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+
+ExternalProject_Add(${proj}
+  ${${proj}_EP_ARGS}
+  # build from Slicer's ITK into the extension's ITK build dir
+  DOWNLOAD_COMMAND ""
+  SOURCE_DIR ${${proj}_SOURCE_DIR}
+  BINARY_DIR ${${proj}_BINARY_DIR}
+  INSTALL_COMMAND ""
+  CMAKE_CACHE_ARGS
+    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+    -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+    -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
+    -DCMAKE_BUILD_TYPE:STRING=Debug
+    # to find ITKConfig.cmake
+    -DITK_DIR:PATH=${ITK_DIR}
+    -DBUILD_TESTING:BOOL=OFF
+    # OpenCV
+    -DOpenCV_INCLUDE_DIRS:STRING=${OpenCV_INCLUDE_DIR};${OpenCV_INCLUDE_DIR}/opencv
+    -DOpenCV_LIB_DIR:PATH=${OpenCV_DIR}/lib
+    -DOpenCV_DIR:PATH=${OpenCV_DIR}/share/OpenCV
+    -DOpenCV_LIB_DIR_DBG:PATH=${OpenCV_DIR}/lib
+    -DOpenCV_LIB_DIR_OPT:PATH=${OpenCV_DIR}/lib
+    -DOpenCV_LIBS:STRING=opencv_core;opencv_videoio;opencv_imgproc
+  DEPENDS ${${proj}_DEPENDENCIES}
+)
+
+#mark_as_superbuild(
+#  VARS ${proj}_SOURCE_DIR:PATH
+#  LABELS "FIND_PACKAGE"
+#  )
---
+++ SuperBuild/External_OpenCV.cmake
@@ -0,0 +1,80 @@
+set(proj OpenCV)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES "")
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+  unset(OpenCV_DIR CACHE)
+  find_package(OpenCV REQUIRED)
+  set(OpenCV_INCLUDE_DIR ${OpenCV_INCLUDE_DIRS})
+  set(OpenCV_LIBRARY ${OpenCV_LIBRARIES})
+endif()
+
+# Sanity checks
+if(DEFINED OpenCV_DIR AND NOT EXISTS ${OpenCV_DIR})
+  message(FATAL_ERROR "OpenCV_DIR variable is defined but corresponds to nonexistent directory")
+endif()
+
+if(NOT DEFINED OpenCV_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+
+  if(NOT DEFINED git_protocol)
+    set(git_protocol "git")
+  endif()
+
+  if(DEFINED ${proj}_SOURCE_DIR)
+    list(APPEND ${proj}_EP_ARGS DOWNLOAD_COMMAND "")
+  else()
+    set(${proj}_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}-source)
+    list(APPEND ${proj}_EP_ARGS
+      URL "https://github.com/Itseez/opencv/archive/3.0.0.tar.gz"
+      URL_MD5 "e6c72f54f7127161b371ef798f34d5c9"
+      )
+  endif()
+  set(${proj}_INSTALL_DIR ${CMAKE_BINARY_DIR}/${Slicer_QTLOADABLEMODULES_LIB_DIR})
+
+  ExternalProject_Message(${proj} "${proj}_SOURCE_DIR:${${proj}_SOURCE_DIR}")
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    SOURCE_DIR ${${proj}_SOURCE_DIR}
+    BINARY_DIR ${proj}-build
+    INSTALL_DIR ${${proj}_INSTALL_DIR}
+    CMAKE_CACHE_ARGS
+      -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
+      -DWITH_IPP:BOOL=OFF
+      # python settings to use the Slicer one
+      -DPYTHON2_EXECUTABLE:PATH=${PYTHON_EXECUTABLE}
+      -DOPENCV_MANGLE_PREFIX:STRING=slicer_opencv_
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+      -DBUILD_TESTING:BOOL=ON
+      -DBUILD_EXAMPLES:BOOL=ON
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+  set(OpenCV_DIR ${${proj}_INSTALL_DIR})
+  set(OpenCV_ROOT ${OpenCV_DIR})
+  set(OpenCV_INCLUDE_DIR ${OpenCV_DIR}/include)
+  if(WIN32)
+    set(OpenCV_LIBRARY ${OpenCV_DIR}/opencv.lib)
+  else()
+    set(OpenCV_LIBRARY ${OpenCV_DIR}/libopencv.a)
+  endif()
+else()
+  # The project is provided using OpenCV_DIR, nevertheless since other projects
+  # may depend on OpenCV, let's add an 'empty' one
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+
+ExternalProject_Message(${proj} "OpenCV_INCLUDE_DIR:${OpenCV_INCLUDE_DIR}")
+ExternalProject_Message(${proj} "OpenCV_LIBRARY:${OpenCV_LIBRARY}")
+if(OPENCV_ROOT)
+  ExternalProject_Message(${proj} "OpenCV_ROOT:${OpenCV_ROOT}")
+endif()
